c0582c3d3116700c1ab3a6ae0f02e283c0aab812
Noman Atique <natique@conestogac.on.ca>
2023-02-27 10:52:18 -0500

    htm

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", function (req, res) {
 
 // setup another route to listen on /about
 app.get("/about", function (req, res) {
-  res.send("<h3>About</h3>");
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 // setup http server to listen on HTTP_PORT
